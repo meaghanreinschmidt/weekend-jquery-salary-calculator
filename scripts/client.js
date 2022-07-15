83b0d32b6dccb62555d16e564a761037a0dd94a7
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-07-15 17:29:48 -0500

    Create a function to calculate the monthly cost of each employee and adds them together

--- scripts/client.js
+++ scripts/client.js
@@ -7,25 +7,54 @@ function readyNow() {
     $('#submit-employee').on('click', submitEmployee);
 }
 
+const budget = 20000
+let employees = [];
+
 function submitEmployee() {
-    console.log('in submitEmployee');
-    // get avlues for name and duration
-    let employeeFirstName = $('#first-name').val();
-    let employeeLastName = $('#last-name').val();
-    let employeeId = $('#id-number').val();
-    let employeeTitle = $('#employee-title').val();
-    let annualSalary = $('#annual-salary').val();
+    // console.log('in submitEmployee');
+    // get values for name and duration
+
+    // decided to make an object instead 
+    // let employeeFirstName = $('#first-name').val();
+    // let employeeLastName = $('#last-name').val();
+    // let employeeId = $('#id-number').val();
+    // let employeeTitle = $('#employee-title').val();
+    // let annualSalary = $('#annual-salary').val();
     
-    console.log(employeeFirstName, employeeLastName, employeeId, employeeTitle, annualSalary);
+    let newEmployee = {
+        employeeFirstName: $('#first-name').val(),
+        employeeLastName: $('#last-name').val(),
+        employeeId: $('#id-number').val(),
+        employeeTitle: $('#employee-title').val(),
+        annualSalary: $('#annual-salary').val()
+    }
+
+    // push the new employee into the array 
+    employees.push(newEmployee);
     // append the values to the DOM
     $('#employee-table').append(`
         <tr>
-            <td>${employeeFirstName}</td>
-            <td>${employeeLastName}</td>
-            <td>${employeeId}</td>
-            <td>${employeeTitle}</td>
-            <td>${annualSalary}</td>
+            <td>${newEmployee.employeeFirstName}</td>
+            <td>${newEmployee.employeeLastName}</td>
+            <td>${newEmployee.employeeId}</td>
+            <td>${newEmployee.employeeTitle}</td>
+            <td>${newEmployee.annualSalary}</td>
     `)
     // clear inputs on the page
     $('input').val('');
+    console.log(calculateMonthlyBudget(employees));
+}
+
+function calculateMonthlyBudget(anArray) {
+    console.log('in calculateMonthlyBudget')
+    let monthlyCost = 0;
+    // loop through salaries array
+    for(let i = 0; i < anArray.length; i++) {
+        // for each employee, add up total of salaries
+        monthlyCost += Number(anArray[i].annualSalary / 12);
+    }
+    return monthlyCost;
+    // console.log adds annual salaries together
+    // next steps: append total salaries
+    // do math to make it monthly salary
 }
